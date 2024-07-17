c65f9603923386c181983b602cb942536e9e8643
Carlos Tovar <carlos.mercury92@gmail.com>
2024-07-17 16:53:17 -0600

    Docstrings added, refactoring some code, and fan-actuators added

--- things_code/activate_actuator.py
+++ things_code/activate_actuator.py
@@ -2,45 +2,75 @@ from machine import ADC, Pin
 import json
 
 
-class Leds:
-    """This class read a value from a light dependent resistor (LDR)"""
+class DigitalPins:
+    """
+    This class controls actuators.
 
-    def __init__(self, parameters):
+    Attributes:
+        actuators (dict): Dictionary to store actuator pin objects keyed by location.
+        actuators_list (list): List of actuators and their corresponding GPIO pins.
+        actuators_list_locations (list): List of actuators locations.
+    """
+    def __init__(self, parameters, type_of_actuator):
         """
-        Initializes a new instance.
-        parameter pin A pin that's connected to an LDR.
-        """
+        Initializes a new instance of the Leds class.
 
-        # initialize ADC (analog to digital conversion)
-        self.leds = {}
-        self.leds_list = parameters['leds']
+        :param parameters: Dictionary containing LED configurations.
+        """
+        self.actuators = {}
+        self.actuators_list = parameters[type_of_actuator]
+        self.actuators_list_locations = []
         self.set_pins()
 
     def set_pins(self):
-
-        for led in self.leds_list:
-            self.leds[led[1]] = Pin(led[0], Pin.OUT)
-            self.leds[led[1]].off()
+        """
+        Sets the pins for LEDs and initializes them to off.
+        """
+        for actuator in self.actuators_list:
+            pin_number, location = actuator
+            self.actuators_list_locations.append(location)
+            self.actuators[location] = Pin(pin_number, Pin.OUT)
+            self.actuators[location].off()
 
     def write(self, location, state):
-        if state:
-            print(location, state, dir(self.leds[location]))
-            self.leds[location].on()
-        if not state:
-            print(location, state)
-            self.leds[location].off()
+        """
+        Turns the LED at the given location on or off based on the state.
+
+        :param location: The location of the LED.
+        :param state: True to turn on the LED, False to turn it off.
+        """
+        if location in self.actuators:
+            if state:
+                self.actuators[location].on()
+            else:
+                self.actuators[location].off()
+        else:
+            print(f"LED at location '{location}' not found.")
 
 
-# Parameters File
-with open(file=r'./parameters_configuration.json', mode='r', encoding='utf-8') as file:
+# Load parameters from file
+with open(file='./parameters_configuration.json', mode='r', encoding='utf-8') as file:
     parameters_ = json.load(file)
 
-if 'actuator' in parameters_ and parameters_['actuator'] == "leds":
-    # initialize a Led
-    leds_iot = Leds(parameters_)
+# Initialize LEDs if the actuator type is 'leds'
+if 'actuator' in parameters_:
+    if parameters_['actuator'] == "leds":
+        leds_iot = DigitalPins(parameters_, 'leds')
+    if parameters_['actuator'] == "fans":
+        leds_iot = DigitalPins(parameters_, 'fans')
 
 
 def activate_actuator(actuator_name, location_, state_):
+    """
+    Activates or deactivates the specified actuator.
 
-    if actuator_name == "leds":
+    :param actuator_name: The name of the actuator (e.g., 'leds').
+    :param location_: The location of the actuator.
+    :param state_: True to activate the actuator, False to deactivate it.
+    """
+    if actuator_name == "leds" and location_ in leds_iot.actuators_list_locations:
         leds_iot.write(location=location_, state=state_)
+    if actuator_name == "fans" and location_ in leds_iot.actuators_list_locations:
+        leds_iot.write(location=location_, state=state_)
+    else:
+        print(f"Actuator '{actuator_name}' at location '{location_}' not found or not supported.")
--- things_code/main.py
+++ things_code/main.py
@@ -1,120 +1,160 @@
 """
-
- To read ldr use
+Instructions:
+ To read LDR use:
     - mosquitto_pub -d -t sensor -m '{"action": {"turn-on":"light", "state": true}}'
- to turn-off readings
+ To turn off readings:
     - mosquitto_pub -d -t sensor -m '{"action": {"turn-on":"light", "state": false}}'
+ To hear readings:
+    - mosquitto_sub -d -t sensors_value
 
- hearing reading
-    -    mosquitto_sub -d -t sensors_value
+ To turn on a LED use:
+    - mosquitto_pub -d -t actuator -m '{"action": {"turn-on":"leds", "location": "sala", "state": true}}'
+ To turn off a LED:
+    - mosquitto_pub -d -t actuator -m '{"action": {"turn-on":"leds", "location": "sala", "state": false}}'
 
- To turn-on a led use
-    - mosquitto_pub -d -t actuator -m '{"action": {"turn-on":"leds", "location": "Comedor", "state": true}}'
- to turn-off a led
-    - mosquitto_pub -d -t actuator -m '{"action": {"turn-on":"leds", "location": "Comedor", "state": false}}'
+    "leds": [[4, "comedor"], [5, "sala"], [19, "dormitorio"], [21, "descanso"], [25, "azotea"],
+    [26, "jardin"], [27, "exterior"]]
+
+ To turn on a fan use:
+    - mosquitto_pub -d -t actuator -m '{"action": {"turn-on":"fans", "location": "cocina", "state": true}}'
+ To turn off a fan:
+    - mosquitto_pub -d -t actuator -m '{"action": {"turn-on":"fans", "location": "cocina", "state": false}}'
+    "fans": [[4, "cocina"]]
 
 """
+
 import json
 import time
 import machine
 import ubinascii
-from umqttsimple import MQTTClient
 import network
 import esp
 import gc
+from umqttsimple import MQTTClient
 from read_sensor import read_sensor
 from activate_actuator import activate_actuator
 
+# Disable OS debugging and collect garbage
 esp.osdebug(None)
 gc.collect()
 
-# Mqtt connection parameters
+# MQTT connection parameters
 client_id_ = ubinascii.hexlify(machine.unique_id())
 
-# Parameters File
-with open(file=r'./parameters_configuration.json', mode='r', encoding='utf-8') as file:
+# Load parameters from file
+with open(file='./parameters_configuration.json', mode='r', encoding='utf-8') as file:
     parameters_ = json.load(file)
 
-# Global Variables
-read_lights = False
-activate_light = [False, {}]
+# Global variables
+read_sensors = False
+activate_actuator_var = [False, {}]
 
 
 def connect_wifi(ssid, password):
+    """
+    Connect to the Wi-Fi network.
+
+    :param ssid: Wi-Fi SSID
+    :param password: Wi-Fi password
+    """
     station = network.WLAN(network.STA_IF)
     station.active(True)
     station.connect(ssid, password)
-
     while not station.isconnected():
         pass
-
     print('Network Connection Successful')
     print(station.ifconfig())
 
 
 def sub_cb(topic, msg):
-    global read_lights, activate_light
+    """
+    MQTT subscription callback function.
 
+    :param topic: The topic of message
+    :param msg: The message payload
+    """
+    global read_sensors, activate_actuator_var
     topic_str = topic.decode('utf-8')
     payload_message = json.loads(msg.decode('utf-8'))
 
     if topic_str == 'sensor':
-        if 'turn-on' in payload_message['action']:
-            if payload_message['action']['turn-on'] == parameters_['sensor']:
-                if payload_message['action']['state']:
-                    read_lights = True
-                if not payload_message['action']['state']:
-                    read_lights = False
+        if 'turn-on' in payload_message['action'] and payload_message['action']['turn-on'] == parameters_['sensor']:
+            read_sensors = payload_message['action']['state']
 
     if topic_str == 'actuator':
-        if 'turn-on' in payload_message['action'] and payload_message['action']['turn-on'] == parameters_['actuator']:
-            activate_light = [True, payload_message]
-        if 'turn-off' in payload_message['action'] and payload_message['action']['turn-off'] == parameters_['actuator']:
-            activate_light = [False, payload_message]
-
-    print(topic, msg, "read lights: ", read_lights, "activate_lights:", activate_light)
+        action_ = payload_message['action']
+        if 'turn-on' in action_ and action_['turn-on'] == parameters_['actuator']:
+            activate_actuator_var = [True, payload_message]
+        elif 'turn-off' in action_ and action_['turn-off'] == parameters_['actuator']:
+            activate_actuator_var = [False, payload_message]
 
 
 def restart_and_reconnect():
+    """
+    Restart and reconnect to the MQTT broker.
+    """
     print('Failed to connect to MQTT broker. Reconnecting...')
     time.sleep(10)
     machine.reset()
 
 
 class LocalMqttClient:
+    """
+    Local MQTT client class to manage MQTT operations.
+    """
     def __init__(self, endpoint, client_id, topics, parameters):
+        """
+        Initialize the LocalMqttClient instance.
+
+        :param endpoint: MQTT broker endpoint
+        :param client_id: Client ID
+        :param topics: List of topics to subscribe to
+        :param parameters: Configuration parameters
+        """
         self.endpoint = endpoint
         self.client_id = client_id
         self.topics = topics
         self.parameters = parameters
 
-        # Create a Mqtt Connection
+        # Create MQTT connection
         self.client = MQTTClient(self.client_id, self.endpoint)
         self.client.set_callback(sub_cb)
         self.client.connect()
         print(f'Connected to MQTT broker: {endpoint}')
 
     def subscribe_to_topics(self):
+        """
+        Subscribe to the specified topics.
+        """
         for topic in self.topics:
-            topic_sub = topic.encode()
-            self.client.subscribe(topic=topic_sub, qos=1)
-            print(f"Subscribe to {topic}")
+            self.client.subscribe(topic.encode(), qos=1)
+            print(f"Subscribed to {topic}")
 
     def publish_to_topics(self, topic: str, input_message: dict):
+        """
+        Publish a message to a specified topic.
+
+        :param topic: Topic to publish to
+        :param input_message: Message payload as a dictionary
+        """
         if input_message:
-            message_json = json.dumps(input_message)
-            topic_pub = topic[0].encode()
-            message_json_byte = message_json.encode()
-            self.client.publish(topic=topic_pub, msg=message_json_byte, qos=1)
+            message_json = json.dumps(input_message).encode()
+            self.client.publish(topic.encode(), msg=message_json, qos=1)
 
     def check_msg(self):
+        """
+        Check for new messages.
+        """
         self.client.check_msg()
 
 
+# Connect to Wi-Fi
 ssid_ = parameters_["ssid"]
 password_ = parameters_["password"]
 connect_wifi(ssid=ssid_, password=password_)
 
+
+# Initialize and connect MQTT client
 client = None
 try:
     client = LocalMqttClient(
@@ -127,28 +167,27 @@ except OSError as err:
     print(err)
     restart_and_reconnect()
 
-while True:
 
+# Main loop
+while True:
     try:
         client.check_msg()
     except OSError as e:
         restart_and_reconnect()
 
-    if read_lights:
+    if read_sensors:
         value = read_sensor(parameters_['sensor'])
         client.publish_to_topics(topic=parameters_["pub_topics"], input_message=value)
-        time.sleep(0.9)
+        time.sleep(1)
 
-    if activate_light[0] and 'action' in activate_light[1]:
-        value = activate_actuator(activate_light[1]['action']['turn-on'],
-                                  activate_light[1]['action']['location'],
-                                  activate_light[1]['action']['state'])
-        activate_light[1] = {}
+    if activate_actuator_var[0] and 'action' in activate_actuator_var[1]:
+        action = activate_actuator_var[1]['action']
+        value = activate_actuator(action['turn-on'], action['location'], action['state'])
+        activate_actuator_var[1] = {}
 
-    if not activate_light[0] and 'action' in activate_light[1]:
-        value = activate_actuator(activate_light[1]['action']['turn-off'],
-                                  activate_light[1]['action']['location'],
-                                  activate_light[1]['action']['state'])
-        activate_light[1] = {}
+    if not activate_actuator_var[0] and 'action' in activate_actuator_var[1]:
+        action = activate_actuator_var[1]['action']
+        value = activate_actuator(action['turn-off'], action['location'], action['state'])
+        activate_actuator_var[1] = {}
 
     time.sleep(0.5)
